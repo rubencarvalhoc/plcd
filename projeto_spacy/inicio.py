c31a82e4b859b862a776d11004ac45b64afa75b7
rubencarvalhoc <a84836@alunos.uminho.pt>
2024-12-26 11:06:06 +0000

    contagem

--- projeto_spacy/inicio.py
+++ projeto_spacy/inicio.py
@@ -12,7 +12,28 @@ import spacy
 from collections import Counter
 from jjcli import *
 
+cont = Counter()
 nlp = spacy.load("pt_core_news_lg")
+cl = clfilter(opt="v", man=__doc__)
 
+def get_standard(text):
+    doc = nlp(text)
 
-cl=clfilter(opt="m:ap:", man=__doc__)
+    for sent in doc.sents:
+        verbs = [token.text for token in sent if token.pos_ == 'VERB']
+        ents = [(ent.text, ent.label_) for ent in sent.ents]
+        noun = [token.text for token in sent if token.pos_ == 'NOUN']
+        adjective = [token.text for token in sent if token.pos_ == 'ADJ']
+
+    feats = {
+        'verb': Counter(verbs),
+        'ents': Counter(ents),
+        'noun': Counter(noun),
+        'adj': Counter(adjective)
+    }
+    return feats
+
+with open('mini.txt', 'r', encoding='utf-8') as teste:
+    texto = teste.read()
+
+print(get_standard(texto))
